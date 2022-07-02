779093e97f85d81f3ca576d1c016d5a9b5bcf1db
Nandu Prasad <prasadnads>
2022-07-03 03:56:07 +0530

    changed settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'kt+0dp=^h9l8bg+a2=in#+8n$&e6958b$znubki$2hf)hr0qq='
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['secure-sands-16359.herokuapp.com', 'carzone.in', 'www.carzoneapp.in']
+ALLOWED_HOSTS = ['secure-sands-16359.herokuapp.com', 'carzoneapp.co', 'www.carzoneapp.co']
 
 
 LOGIN_REDIRECT_URL = 'dashboard'
